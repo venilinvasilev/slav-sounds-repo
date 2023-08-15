ffc6cfb27d70699bf204736b4bf9244bd5245151
phatyg <venilin.vasilev@gmail.com>
2023-08-15 20:48:43 +0300

    Generated slav-sounds-record-pool-web application

---
+++ apps/slav-sounds-record-pool-web/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'slav-sounds-record-pool-web',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/slav-sounds-record-pool-web',
+};
